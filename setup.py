9e90ed7eeccd43e2ba9403c5019484a8532d4335
Brendan Griffen <brednan.f.griffen@gmail.com>
2023-08-07 22:55:09 +1000

    updated packages

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='wombat',
-    version='0.1',
-    description='A Python toolkit designed help answer geospatial related questions about Australian cities and regions.',
-    author='Brendan Griffen',
-    author_email='@brendangriffen',
-    packages=['wombat'],
-    install_requires=['dependency1', 'dependency2'],
+    name="wombat-python",
+    version="0.1",
+    description="A Python toolkit designed help answer geospatial related questions about Australian cities and regions.",
+    author="Brendan Griffen",
+    author_email="@brendangriffen",
+    packages=["wombat-python"],
+    # install_requires=['dependency1', 'dependency2'],
 )
