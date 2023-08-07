38559e9fb935e90b0ed1687fe28d81ba15899448
Brendan Griffen <brendan.f.griffen@gmail.com>
2023-08-07 22:53:01 +1000

    first commit

---
+++ wombat/__init__.py
@@ -0,0 +1,8 @@
+__author__ = "Brendan Griffen"
+__version__ = "0.1.0"
+__email__ = "@brendangriffen"
+
+# # populate the namespace so top level imports work
+
+# from . import datasets,helpers,elevation,maps,models,scraping
+# from wombat.wombat import X as X
