0fa21063424fbff9b9f88449f9d71f96cf310a1c
Brendan Griffen <brendan.f.griffen@gmail.com>
2023-08-10 00:26:09 +1000

    refactored to run multi-city

--- wombat/urbanity.py
+++ wombat/urbanity.py
@@ -2,12 +2,22 @@ import wombat.helper as helper
 import urbanity as urb
 
 class Urbanity:
-    def __init__(self,city,zoom=10):
+    def __init__(self,city,zoom):
         self.city = city
+        self.zoom = zoom
         self.map = urb.Map(country="Australia",zoom=zoom)
         self.map.center = helper.caplatlon[city]
         
-    def run(self,gdf):
+    def define_region(self,gdf):
         gdf.to_file('tmp_region.geojson', driver='GeoJSON')
         self.map.add_polygon_boundary('tmp_region.geojson')
+    
+    def run(self,pbf_path,graph_attr=True,building_attr=True,pop_attr=True,svi_attr=True,poi_attr=True,bandwith=200):
+        self.graph, self.nodes, self.edges = self.map.get_street_network(filepath=pbf_path,location=self.city, 
+                                               graph_attr=True,
+                                               building_attr=True, 
+                                               pop_attr=True, 
+                                               svi_attr=True,
+                                               poi_attr=True,
+                                               bandwidth=200)
         
--- wombat/wombat.py
+++ wombat/wombat.py
@@ -1,17 +1,33 @@
 import wombat.helper as helper
 from wombat.urbanity import Urbanity
 from wombat.datasets import Boundary
-
+import os
 class Wombat:
     def __init__(self,dataset_path,zoom=10):
         self.zoom = zoom
         self.dataset_path = dataset_path
-        self.Boundary = Boundary(dataset_path)
-        self.Urbanity = Urbanity(dataset_path,self.zoom)
         
-    def set_city(city):
+    def set_city(self,city):
         self.city = city
+        self.Boundary = Boundary(self.dataset_path)
         self.Boundary.load(city)
+        self.Urbanity = Urbanity(city=city,zoom=self.zoom)
         
-    def run_urbanity(self):
-        self.Urbanity.run(self.Boundary.gdf)
+    def run_urbanity(self,graph_attr=True,
+                     building_attr=True,
+                     pop_attr=True,
+                     svi_attr=True,
+                     poi_attr=True,
+                     bandwith=200):
+
+        #self.Urbanity.define_region(self.Boundary.gdf)
+        
+        self.pbf_path = os.path.join(self.dataset_path,"pbf",self.city+".osm.pbf")
+        print(self.pbf_path)
+        
+        self.Urbanity.run(pbf_path=self.pbf_path,graph_attr=graph_attr,
+                          building_attr=building_attr,
+                          pop_attr=pop_attr,
+                          svi_attr=svi_attr,
+                          poi_attr=poi_attr,
+                          bandwith=200)
