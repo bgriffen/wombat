da802deef996227c2f4e52d8854dc930f8347682
Brendan Griffen <brednan.f.griffen@gmail.com>
2023-08-10 00:26:45 +1000

    first commit

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name="wombat-python",
+    name="wombat",
     version="0.1",
     description="A Python toolkit designed help answer geospatial related questions about Australian cities and regions.",
     author="Brendan Griffen",
     author_email="@brendangriffen",
-    packages=["wombat-python"],
+    packages=["wombat"],
     # install_requires=['dependency1', 'dependency2'],
 )
