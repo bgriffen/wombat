835c2dda8cf362fa7dcd16b561855e22afa7f39f
Brendan Griffen <brednan.f.griffen@gmail.com>
2023-08-11 00:22:19 +1000

    added buildings class

--- wombat/wombat.py
+++ wombat/wombat.py
@@ -1,33 +1,40 @@
 import wombat.helper as helper
 from wombat.urbanity import Urbanity
 from wombat.datasets import Boundary
+from wombat.buildings import Buildings
 import os
+
 class Wombat:
+    # Initializing Wombat class with dataset_path and zoom level defaulting to 10
     def __init__(self,dataset_path,zoom=10):
         self.zoom = zoom
         self.dataset_path = dataset_path
+        self.cities = sorted(helper.caplatlon.keys())
         
-    def set_city(self,city):
+    # Method to load boundary data into the instance  
+    def load(self,boundary_dataset):
+        print("Loading boundary data...")
+        self.Boundary = Boundary(self.dataset_path,dataset=boundary_dataset)
+        
+    # Method to set the city for analysis and initialize Urbanity object instance   
+    def set_city(self,city,radius=None):
         self.city = city
-        self.Boundary = Boundary(self.dataset_path)
-        self.Boundary.load(city)
-        self.Urbanity = Urbanity(city=city,zoom=self.zoom)
+        self.Boundary.set_city(city,radius)
+        self.Urbanity = Urbanity(dataset_path=self.dataset_path,city=city,zoom=self.zoom)
+        self.Buildings = Buildings(dataset_path=self.dataset_path,city=city)
         
+    # Method to run urbanity analysis with various attributes and a fixed bandwidth of 200   
     def run_urbanity(self,graph_attr=True,
                      building_attr=True,
                      pop_attr=True,
                      svi_attr=True,
                      poi_attr=True,
                      bandwith=200):
-
-        #self.Urbanity.define_region(self.Boundary.gdf)
-        
-        self.pbf_path = os.path.join(self.dataset_path,"pbf",self.city+".osm.pbf")
-        print(self.pbf_path)
-        
-        self.Urbanity.run(pbf_path=self.pbf_path,graph_attr=graph_attr,
+  
+        self.Urbanity.run(graph_attr=graph_attr,
                           building_attr=building_attr,
                           pop_attr=pop_attr,
                           svi_attr=svi_attr,
                           poi_attr=poi_attr,
                           bandwith=200)
+        
