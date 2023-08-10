c373c795f51d0d30f7ab0734f06e9d4563e3f450
Brendan Griffen <brendan.f.griffen@gmail.com>
2023-08-11 01:17:16 +1000

    refactor the load

--- wombat/wombat.py
+++ wombat/wombat.py
@@ -1,25 +1,26 @@
 import wombat.helper as helper
 from wombat.urbanity import Urbanity
-from wombat.datasets import Boundary
+from wombat.boundary import Boundary
 from wombat.buildings import Buildings
 import os
+import warnings
+from shapely.errors import ShapelyDeprecationWarning
+warnings.filterwarnings("ignore", category=ShapelyDeprecationWarning) 
+warnings.filterwarnings("ignore", category=UserWarning) 
+warnings.filterwarnings('ignore', category=FutureWarning)
 
 class Wombat:
     # Initializing Wombat class with dataset_path and zoom level defaulting to 10
-    def __init__(self,dataset_path,zoom=10):
+    def __init__(self,dataset_path,boundary_dataset="SA3_2021_AUST_GDA2020",zoom=10):
         self.zoom = zoom
         self.dataset_path = dataset_path
         self.cities = sorted(helper.caplatlon.keys())
-        
-    # Method to load boundary data into the instance  
-    def load(self,boundary_dataset):
-        print("Loading boundary data...")
-        self.Boundary = Boundary(self.dataset_path,dataset=boundary_dataset)
+        self.boundary_dataset = boundary_dataset
         
     # Method to set the city for analysis and initialize Urbanity object instance   
     def set_city(self,city,radius=None):
         self.city = city
-        self.Boundary.set_city(city,radius)
+        self.Boundary = Boundary(self.dataset_path,dataset=self.boundary_dataset,city=self.city)
         self.Urbanity = Urbanity(dataset_path=self.dataset_path,city=city,zoom=self.zoom)
         self.Buildings = Buildings(dataset_path=self.dataset_path,city=city)
         
