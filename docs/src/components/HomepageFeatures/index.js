775d8454106582b689bd24850e6e14c1c2089997
Brendan Griffen <brednan.f.griffen@gmail.com>
2023-08-17 00:53:11 +1000

    updated subtitles

--- docs/src/components/HomepageFeatures/index.js
+++ docs/src/components/HomepageFeatures/index.js
@@ -8,28 +8,27 @@ const FeatureList = [
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Wombat was designed from the ground up to be easily installed and
+        used to answer your geospatial questions about Australian cities quickly.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'No Nonsense',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Wombat is designed to be used by data scientists and tinkerers who 
+        want a streamlined path to answering basic geospatial questions about their capital cities.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Powered By Python',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        No leviathan infrastructure needs, inter-connected databases, or brittle package dependencies - just Python.
       </>
     ),
   },
