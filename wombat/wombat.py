4fcfcc0c5afa30e8ba5700c29aa9e5bbd7ba9def
Brendan Griffen <brendan.f.griffen@gmail.com>
2023-08-25 00:28:07 +1000

    changed to inheriting map in Wombat class vs in visualisation class

--- wombat/wombat.py
+++ wombat/wombat.py
@@ -1,50 +1,58 @@
 import wombat.helper as helper
-from wombat.urbanity import Urbanity
+#from wombat.urbanity import Urbanity
 from wombat.boundary import Boundary
 from wombat.buildings import Buildings
 from wombat.visualise import Viz
 from wombat.schools import Schools
-from wombat.datasets import Datasets
+from wombat.datasets import Datasets,City
 from wombat.elevation import Elevation
 import os
 import warnings
 from shapely.errors import ShapelyDeprecationWarning
-import ipyleaflet
-from urbanity import utils
-
+#import ipyleaflet
 warnings.filterwarnings("ignore", category=ShapelyDeprecationWarning) 
 warnings.filterwarnings("ignore", category=UserWarning) 
 warnings.filterwarnings('ignore', category=FutureWarning)
 warnings.filterwarnings('ignore')
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 from wombat.datasets import City
 
 import platform
-class Wombat:
-    # Initializing Wombat class with dataset_path and zoom level defaulting to 10
-    def __init__(self,dataset_path,zoom=10):
+import leafmap.foliumap as leafmap
 
-        self.zoom = zoom
+class Wombat(leafmap.Map):
+    # Initializing Wombat class with dataset_path and zoom level defaulting to 10
+    def __init__(self,**kwargs):
+    
+        super().__init__(**kwargs)
+        
+        self.zoom = 10
         self.dataset_path = dataset_path
         self.Datasets = Datasets(self.dataset_path)
         self.Boundary = Boundary(self.dataset_path)
         self.Elevation = Elevation(self.dataset_path)
+        self.map = leafmap.Map()
         
     # Method to set the city for analysis and initialize Urbanity object instance
     def set_city(self,city,radius=None):
         self.Boundary = Boundary(self.dataset_path,city=city)
-        self.Urbanity = Urbanity(dataset_path=self.dataset_path,city=city)
+        #self.Urbanity = Urbanity(dataset_path=self.dataset_path,city=city)
         self.Buildings = Buildings(dataset_path=self.dataset_path,city=city)
-        self.Viz = Viz(dataset_path=self.dataset_path,city=city,zoom=self.zoom)
+        #self.Viz = Viz(dataset_path=self.dataset_path,city=city,zoom=self.zoom)
         self.Schools = Schools(dataset_path=self.dataset_path,city=city)
         self.Elevation = Elevation(dataset_path=self.dataset_path,city=city)
-        
-    def load(self):
-        self.Viz.load()
+        self.City = self.City = City(city)
+        self.Datasets = Datasets(self.dataset_path,city=city)
+        self.map = leafmap.Map(location=[self.City.lat,self.City.lon],zoom_start=self.zoom)
         
     def download_osm(self):
         self.Datasets.download_osm()
 
+    def add_sa3_boundary(self):
+        self.map = leafmap.Map(location=[self.City.lat,self.City.lon],zoom_start=self.zoom)
+        self.map.add_gdf(self.Boundary.gdf.copy(),layer_name='[ SA3 Boundary ] %s'%self.City.name)
+    
     # Method to run urbanity analysis with various attributes and a fixed bandwidth of 200   
     def run_urbanity(self,graph_attr=True,
                      building_attr=True,
