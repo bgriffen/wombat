45ba075b5a8896662d67f5fdcf10ae7ef7db1935
Brendan Griffen <brendan.f.griffen@gmail.com>
2023-08-25 23:59:41 +1000

    updated

--- wombat/boundary.py
+++ wombat/boundary.py
@@ -4,6 +4,7 @@ from shapely.geometry import Point
 import wombat.helper as helper
 import os
 from wombat.datasets import Datasets,City
+import glob
 
 def polygons_within_radius(gdf,lat,lon,radius):
     """Find polygons within a given radius of a center point.
@@ -35,24 +36,26 @@ class Boundary(Datasets):
         super().__init__(dataset_path,city)
         self.dataset_path = dataset_path
         self.dataset = "SA3_2021_AUST_GDA2020"
-
+        
         self.folder = os.path.join(dataset_path,"boundary")
-        self.filename = os.path.join(self.folder,"%s.geojson"%self.dataset)
+        self.filename_country = os.path.join(self.folder,"%s.geojson"%self.dataset)
+        boundary_files = glob.glob(os.path.join(self.folder,"*AUST*.geojson"))
+        self.available_boundaries = sorted(list(set([os.path.basename(f) for f in boundary_files])))
         
         if city is not None:
             self.City = City(city)
+            self.filename_city = os.path.join(self.folder,self.dataset+"_"+f"{self.City.name}_boundary.geojson")
             print("Setting:",self.City.name)
-            fout = os.path.join(self.folder,self.dataset+"_"+f"{self.City.name}_boundary.geojson")
-            if not os.path.exists(fout):
-                self.gdf_full = gpd.read_file(self.filename)
+            if not os.path.exists(self.filename_city):
+                self.gdf_full = gpd.read_file(self.filename,engine='pyogrio')
                 if self.City.name == "Canberra":  # Hacky handling to sort out beloved ACT/Canberra
                     mask_city = self.gdf_full['GCC_NAME21'].str.contains("Australian Capital Territory")
                 else:
                     mask_city = self.gdf_full['GCC_NAME21'].str.contains(self.City.name)
                 self.gdf = self.gdf_full[mask_city]
-                self.gdf.to_file(fout)
+                self.gdf.to_file(self.filename_city,engine='pyogrio')
             else:
-                self.gdf = gpd.read_file(fout)
+                self.gdf = gpd.read_file(self.filename_city,engine='pyogrio')
         
             #if radius is not None:
             #    self.set_radius(radius)
@@ -60,7 +63,7 @@ class Boundary(Datasets):
             self.names = sorted(list(set(self.gdf['SA3_NAME21'])))
     
     def load_states_territories(self):
-        self.gdf_states_territories = gpd.read_file(self.boundary_path_states_territories)
+        self.gdf_states_territories = gpd.read_file(self.boundary_path_states_territories,engine='pyogrio')
         
     def set_radius(self,radius=10):
         self.gdf = polygons_within_radius(self.gdf,self.City.lat,self.City.lon,radius)
@@ -76,4 +79,4 @@ class OpenStreetMap:
             boundary = Boundary(self.dataset_path)
             boundary.load()
             boundary.set_radius(20)
-            boundary.gdf.to_file(os.path.join(self.pbf_path,city.name+".geojson"), driver='GeoJSON')
+            boundary.gdf.to_file(os.path.join(self.pbf_path,city.name+".geojson"), driver='GeoJSON',engine='pyogrio')
